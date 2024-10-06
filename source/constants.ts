6b3658852b23b837024966175d4ca566a1b4a2fc
Vsevolod Pletnev <vseplet@gmail.com>
2024-10-06 16:15:05 +0700

    fixes

--- source/constants.ts
+++ source/constants.ts
@@ -3,7 +3,7 @@ import { fetchJSON } from "$/helpers";
 import { colors } from "@std/colors";
 
 const remoteDenoJson = await fetchJSON(
-  `https://raw.githubusercontent.com/vseplet/PP-01/main/deno.json`,
+  `https://raw.githubusercontent.com/devexp-pro/cli/refs/heads/develop/deno.json`,
 ) as unknown as typeof localDenoJson;
 
 const permissionEnv = Deno.permissions.querySync({ name: "env" }).state;
@@ -16,7 +16,7 @@ export const VERSION = localDenoJson["version"];
 export const REMOTE_VERSION = remoteDenoJson["version"] || VERSION;
 
 export const ENTRYPOINT_SOURCE_URL =
-  `https://raw.githubusercontent.com/vseplet/PP-01/main/source/agent/main.ts`;
+  `https://raw.githubusercontent.com/devexp-pro/cli/refs/heads/develop/source/main.ts`;
 
 export const SERVICE_DOMAIN = IS_DEVELOP ? "localhost:4000" : "devexp.cloud";
 
