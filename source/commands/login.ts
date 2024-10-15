dcefd3f9bc70cbab1b2d12c3ae2e72ad87215a9b
Artem <artp80@gmail.com>
2024-10-15 15:23:40 +0700

    add: login/logout

--- source/commands/login.ts
+++ source/commands/login.ts
@@ -1,41 +1,50 @@
 import { Command } from "@cliffy/command";
 import { open } from "https://deno.land/x/open/index.ts";
+import { kv } from "$/shared";
 
-// Подключаемся к локальному хранилищу KV
-const kv = await Deno.openKv("ololo");
-
-// Команда для авторизации
 export const login = new Command()
   .description("Login via GitHub OAuth")
   .action(async () => {
     try {
-      // Генерация соли (UUID)
+      const sessionData = kv.list<{}>({ prefix: ["auth"] });
+      let sessionDataArray = <any> [];
+      for await (const entry of sessionData) {
+        sessionDataArray.push(entry);
+      }
+
+      if (sessionDataArray.length > 0) {
+        const sessionId = sessionDataArray[0].key[1];
+        const githubUsername = sessionDataArray[0].value.github_username;
+
+        console.log(
+          `You are already logged in as ${githubUsername} with sessionId: ${sessionId}.`,
+        );
+        return;
+      }
+
       const authToken = crypto.randomUUID();
       console.log(`Generated UUID for auth_token: ${authToken}`);
 
-      // Открываем браузер с URL для авторизации через GitHub
       const url = `http://localhost:8000/login?auth_token=${authToken}`;
       console.log(`Opening browser with URL: ${url}`);
       await open(url, { wait: false });
       console.log("Открыт браузер для авторизации через GitHub...");
 
-      // Отправляем запрос на сервер с этим UUID, который будет ожидать завершения авторизации
-      console.log(
-        `Sending wait-for-login request with auth_token: ${authToken}`,
-      );
       const result = await fetch(
         `http://localhost:8000/wait-for-login?auth_token=${authToken}`,
       );
 
       if (result.ok) {
         const responseJson = await result.json();
-        const { sessionId, githubUsername } = responseJson;
+        const { sessionId, githubUsername, uuid } = responseJson;
 
-        // Сохраняем sessionId и githubUsername в локальном KV
         console.log(
           `Received from server: sessionId: ${sessionId}, githubUsername: ${githubUsername}`,
         );
-        await kv.set(["auth", sessionId], { github_username: githubUsername });
+        await kv.set(["auth", sessionId], {
+          github_username: githubUsername,
+          uuid,
+        });
         console.log(
           `Успешная авторизация! Username: ${githubUsername}, sessionId: ${sessionId}`,
         );
--- source/commands/logout.ts
+++ source/commands/logout.ts
@@ -1,6 +1,35 @@
 import { Command } from "@cliffy/command";
+import { kv } from "$/shared";
+interface SessionData {
+  github_username: string;
+}
 
 export const logout = new Command()
-  .description("logout subcommand description")
-  .action(async (_options: any, ..._args: any) => {
+  .description("Logout from the current session")
+  .action(async () => {
+    const sessionData = kv.list<SessionData>({ prefix: ["auth"] });
+    let sessionDataArray: Array<{ key: string[]; value: SessionData }> = [];
+
+    for await (const entry of sessionData) {
+      //@ts-ignore
+      sessionDataArray.push(entry);
+    }
+
+    if (sessionDataArray.length === 0) {
+      console.log("No active session found. You are not logged in.");
+      return;
+    }
+
+    const sessionId = sessionDataArray[0].key[1];
+
+    const result = await fetch(
+      `http://localhost:8000/logout?session_id=${sessionId}`,
+    );
+
+    if (result.ok) {
+      await kv.delete(["auth", sessionId]);
+      console.log("Logged out successfully!");
+    } else {
+      console.error("Error during logout");
+    }
   });
