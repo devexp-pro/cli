1693367fb00f69d94e7538029aacb65f537ca024
Artem <artp80@gmail.com>
2024-10-23 14:36:13 +0700

    fix: create env

--- source/tools/vault/commands/create_env.ts
+++ source/tools/vault/commands/create_env.ts
@@ -1,30 +1,20 @@
 import { Command } from "../deps.ts";
 import { createClient, getCurrentProject } from "../api.ts";
 
-import { green, red } from "../deps.ts";
-import { Input } from "@cliffy/prompt/input";
-
 export function createEnvCommand() {
   return new Command()
     .description("Создать новое окружение для выбранного проекта.")
-    .action(async () => {
+    .arguments("<envName:string>") // Добавляем аргумент для имени окружения
+    .action(async (_options: any, envName: string) => {
       try {
         const project = await getCurrentProject();
 
         if (!project) {
           console.error(
-            red(
-              "Проект не выбран. Пожалуйста, выберите проект перед созданием окружения.",
-            ),
+            "Проект не выбран. Пожалуйста, выберите проект перед созданием окружения.",
           );
           return;
         }
-
-        const envName = await Input.prompt({
-          message: "Введите имя нового окружения:",
-          validate: (value) =>
-            value.length > 0 ? true : "Имя окружения не может быть пустым.",
-        });
 
         const client = await createClient();
         const response = await client.call("createEnvironment", [
@@ -32,19 +22,17 @@ export function createEnvCommand() {
           envName,
         ]);
 
-        if (response.success) {
-          console.log(
-            green(
-              `Окружение '${envName}' успешно создано для проекта '${project}'.`,
-            ),
-          );
-        } else {
-          console.error(
-            red(`Не удалось создать окружение. Ошибка: ${response.message}`),
+        if (!response.success) {
+          throw new Error(
+            `Не удалось создать окружение. Ошибка: ${response.message}`,
           );
         }
+
+        console.log(
+          `Окружение '${envName}' успешно создано для проекта '${project}'.`,
+        );
       } catch (error) {
-        console.error(red(`Ошибка: ${(error as Error).message}`));
+        console.error(`Ошибка: ${(error as Error).message}`);
       }
     });
 }
