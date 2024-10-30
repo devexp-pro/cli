f533ca8286b3a457e17bb1900541cce1bdd2ff2d
Artem <artp80@gmail.com>
2024-10-30 16:43:38 +0700

    fix: login

--- source/kv.ts
+++ source/kv.ts
@@ -1,21 +1,21 @@
+// source/kv.ts
+
 import { SessionData } from "$/types";
+import { config } from "$/shared";
+
+
+
 
 export const kv = Deno.env.get("DEV")
-  ? await Deno.openKv("ololo")
+  ? await Deno.openKv("local-kv")
   : await Deno.openKv();
 
 export async function getSessionID(): Promise<string | null> {
   try {
-    // Получаем текущие данные сессии по ключу ["auth", "session"]
-    const sessionData = await kv.get<{ sessionId: string }>([
-      "auth",
-      "session",
-    ]);
+    const sessionData = await kv.get<{ sessionId: string }>(["auth", "session"]);
 
     if (sessionData.value) {
-      const sessionId = sessionData.value.sessionId;
-
-      return sessionId;
+      return sessionData.value.sessionId;
     }
 
     console.log("Активная сессия не найдена.");
@@ -28,10 +28,7 @@ export async function getSessionID(): Promise<string | null> {
 
 export async function getSession(): Promise<SessionData | null> {
   try {
-    const sessionData = await kv.get<SessionData>([
-      "auth",
-      "session",
-    ]);
+    const sessionData = await kv.get<SessionData>(["auth", "session"]);
 
     if (sessionData.value) {
       return sessionData.value;
@@ -42,5 +39,16 @@ export async function getSession(): Promise<SessionData | null> {
   } catch (error) {
     console.error("Ошибка при попытке получить sessionId:", error);
     return null;
+  }
+}
+
+
+export async function setSessionWithExpiration(data: SessionData): Promise<void> {
+  try {
+    const sessionTTL = config.data.features.auth.times.sessionTTL;
+    console.log(sessionTTL)
+    await kv.set(["auth", "session"], data, { expireIn: sessionTTL });
+  } catch (error) {
+    console.error("Ошибка при сохранении сессии:", error);
   }
 }
