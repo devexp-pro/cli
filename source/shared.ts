17ee229f68854194936f8149535e8d6b2f6b070f
Vsevolod Pletnev <vseplet@gmail.com>
2024-10-31 02:00:52 +0700

    добавил костыль для обхода выбора конфига

--- source/shared.ts
+++
@@ -1,6 +0,0 @@
-import Tuner from "@artpani/tuner";
-import { BaseCfgType } from "../config/base.tuner.ts";
-
-export const config = await Tuner.use.loadConfig<BaseCfgType>({
-  configDirPath: "config",
-});
