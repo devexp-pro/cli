fa57bfc3deea18bad8d6d725f21feccdcb7cb415
Vsevolod Pletnev <vseplet@gmail.com>
2024-11-03 03:11:21 +0700

    init flow

--- source/tools/flow/commands/init.ts
+++ source/tools/flow/commands/init.ts
@@ -1,8 +1,16 @@
 import { Command } from "@cliffy/command";
 
-export const init = new Command()
+const action = async () => {
+};
+
+const command = new Command()
   .name("init")
   .description("init project description")
   .action(async (_options: any, ..._args: any) => {
     Deno.exit(0);
   });
+
+export default {
+  action,
+  command,
+};
--- source/tools/flow/commands/run.ts
+++ source/tools/flow/commands/run.ts
@@ -1,8 +1,23 @@
 import { Command } from "@cliffy/command";
 
-export const run = new Command()
+const action = async () => {
+};
+
+const command = new Command()
   .name("run")
+  .usage("[path]")
+  .arguments("[path] [output:string]")
   .description("run subcommand description")
-  .action(async (_options: any, ..._args: any) => {
+  .action(async (options: any, ...args: any) => {
+    console.log(options);
+    console.log(args);
+    for (const dir of Deno.readDirSync(Deno.cwd())) {
+      if (dir.isFile) console.log(dir.name);
+    }
     Deno.exit(0);
   });
+
+export default {
+  action,
+  command,
+};
