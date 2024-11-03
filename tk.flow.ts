a3283d055bfda2dfc376a186bdea1d79cd12c2a4
Vsevolod Pletnev <vseplet@gmail.com>
2024-11-03 18:31:45 +0700

    засетапил интерактивный режим

--- tk.flow.ts
+++
@@ -1,9 +0,0 @@
-
-import { task } from "jsr:@vseplet/shibui@0.4.36/core";
-
-export default task()
-  .name`tk`
-  .do(async ({ finish, pots, log }) => {
-    log.dbg('run do function...');
-    return finish();
-  });
