2e83827d3b54dbf929b6f036635df24aaf7072e9
Vsevolod Pletnev <vseplet@gmail.com>
2024-11-12 04:50:45 +0700

    fixes

--- source/commands/entry.ts
+++ source/commands/entry.ts
@@ -9,23 +9,19 @@ import {
   VERSION,
 } from "$/constants";
 
-import toolLlm from "$/tools/llm";
 import toolTunnel from "$/tools/tunnel";
 import toolConfig from "$/tools/config";
 import toolVault from "$/tools/vault";
 import toolGit from "$/tools/git";
 import toolFlow from "$/tools/flow";
-import toolHyper from "$/tools/hyper";
-import toolTemplate from "$/tools/template";
 import toolAlias from "$/tools/alias";
-import toolTerm from "$/tools/term";
-import toolRadio from "$/tools/radio";
 import toolClip from "$/tools/clip";
 
 import { dash } from "./dash.ts";
 import { intro } from "./intro.ts";
 import { setup } from "./setup/mod.ts";
 import { upgrade } from "./setup/upgrade.ts";
+
 export const entry = new Command()
   .name("dx")
   .usage("[command]")
@@ -44,18 +40,15 @@ export const entry = new Command()
     entry.showHelp();
     Deno.exit();
   })
-  // .command("llm", toolLlm)
+  // tools
   .command("tunnel", toolTunnel)
   .command("config", toolConfig)
   .command("vault", toolVault)
   .command("flow", toolFlow.tool)
-  // .command("template", toolTemplate)
   .command("alias", toolAlias)
-  // .command("hyper", toolHyper)
   .command("git", toolGit)
-  // .command("term", toolTerm)
-  // .command("radio", toolRadio)
   .command("clip", toolClip)
+  // commands
   .command("dash", dash)
   .command("intro", intro)
   .command("setup", setup);
