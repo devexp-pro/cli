91952a0fc8bc9202fb02c2404d27fa2995554d31
Vsevolod Pletnev <vseplet@gmail.com>
2024-11-15 00:24:16 +0700

    Update config.ts

--- source/commands/config.ts
+++ source/commands/config.ts
@@ -1,11 +1,15 @@
 import { Command } from "@cliffy/command";
 import { config } from "$/constants";
+import * as constants from "$/constants";
 
 export const cfg = new Command()
   .name("cfg")
   .description("show config")
   .hidden()
   .action(async () => {
-    console.log(JSON.stringify(config, null, 2));
+    // console.log(JSON.stringify(config, null, 2));
+    console.log("--------------------------------");
+    console.log(JSON.stringify(constants, null, 2));
+    console.log("--------------------------------");
     Deno.exit();
   });
