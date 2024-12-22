404d031789194518761e9b23528956f20b98d967
Vsevolod Pletnev <vseplet@gmail.com>
2024-12-22 19:21:32 +0700

    c

--- source/commands/cfg.ts
+++ source/commands/cfg.ts
@@ -1,6 +1,7 @@
 import { Command } from "@cliffy/command";
 import { config } from "$/providers/config.ts";
 import * as constants from "$/constants";
+import * as version from "$/providers/version.ts";
 
 export const cfg = new Command()
   .name("cfg")
@@ -11,6 +12,7 @@ export const cfg = new Command()
     console.log(JSON.stringify(config, null, 2));
     console.log("--------------------------------");
     console.log(JSON.stringify(constants, null, 2));
+    console.log(JSON.stringify(version, null, 2));
     console.log(JSON.stringify(import.meta, null, 2));
     console.log("--------------------------------");
     Deno.exit();
