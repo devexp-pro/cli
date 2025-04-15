0e3c20e280bbbba710500c0409feb14bd92c573a
Vsevolod Pletnev <vseplet@gmail.com>
2025-04-16 05:48:19 +0700

    add llm to spotlight

--- source/tools/llm/mod.ts
+++ source/tools/llm/mod.ts
@@ -5,6 +5,9 @@ import { LMStudioClient } from "npm/@lmstudio/sdk";
 import { colors } from "@std/colors";
 import tui from "$/providers/tui/mod.ts";
 import { kv } from "$/repositories/kv.ts";
+import { Input } from "@cliffy/prompt/input";
+
+const spotlight = [];
 
 const setup = new Command()
   .name("setup")
@@ -18,6 +21,54 @@ const setup = new Command()
 
     Deno.exit();
   });
+
+const llmPrompt = async (
+  question: string,
+  max_tokens: number | undefined = undefined,
+) => {
+  const lmstudioOptions =
+    (await kv.get<{ baseUrl: string }>(["tool", "llm", "lmstudio"])).value ||
+    {
+      baseUrl: "ws://127.0.0.1:1234",
+    };
+
+  const client = new LMStudioClient(lmstudioOptions);
+  const model = await client.llm.model();
+  const modelInfo = await model.getModelInfo();
+  const name = `  ${modelInfo.displayName}: `;
+  let lineWidth = name.length;
+
+  console.log();
+  tui.print(name);
+
+  for await (
+    const fragment of model.respond(
+      question,
+      max_tokens
+        ? {
+          maxTokens: max_tokens,
+        }
+        : {},
+    )
+  ) {
+    const content = fragment.content;
+
+    try {
+      lineWidth += content.length;
+      if (lineWidth > 80) {
+        tui.print("\n");
+        lineWidth = 0;
+        tui.print(colors.yellow(content.trimStart()));
+      } else {
+        tui.print(colors.yellow(content));
+      }
+    } catch (e) {
+      // TODO: вот тут должен работать логгер
+      // console.log(typeof content);
+      // console.log(e);
+    }
+  }
+};
 
 const tool = new Command();
 if (config.data.tools.llm.hidden) tool.hidden();
@@ -34,49 +85,9 @@ tool
       Deno.exit();
     }
 
-    const lmstudioOptions =
-      (await kv.get<{ baseUrl: string }>(["tool", "llm", "lmstudio"])).value ||
-      {
-        baseUrl: "ws://127.0.0.1:1234",
-      };
-
-    const client = new LMStudioClient(lmstudioOptions);
-    const model = await client.llm.model();
-    const modelInfo = await model.getModelInfo();
     const [question, max_tokens] = args;
-    const name = `  ${modelInfo.displayName}: `;
-    let lineWidth = name.length;
 
-    console.log();
-    tui.print(name);
-
-    for await (
-      const fragment of model.respond(
-        question,
-        max_tokens
-          ? {
-            maxTokens: max_tokens,
-          }
-          : {},
-      )
-    ) {
-      const content = fragment.content;
-
-      try {
-        lineWidth += content.length;
-        if (lineWidth > 80) {
-          tui.print("\n");
-          lineWidth = 0;
-          tui.print(colors.yellow(content.trimStart()));
-        } else {
-          tui.print(colors.yellow(content));
-        }
-      } catch (e) {
-        // TODO: вот тут должен работать логгер
-        // console.log(typeof content);
-        // console.log(e);
-      }
-    }
+    await llmPrompt(question, max_tokens);
 
     console.log("\n");
 
@@ -84,6 +95,29 @@ tool
   })
   .command("setup", setup);
 
+spotlight.push({
+  tag: "cmd",
+  name: "llm help",
+  stringForSearch: "llm help",
+  description: "Show help for llm tool",
+  handler: async () => {
+    tool.showHelp();
+    Deno.exit();
+  },
+});
+
+spotlight.push({
+  tag: "cmd",
+  name: "llm ask",
+  stringForSearch: "llm ask",
+  description: tool.getDescription(),
+  handler: async () => {
+    const prompt = await Input.prompt(`Type your prompt:`);
+    await llmPrompt(prompt);
+  },
+});
+
 export default {
   tool,
+  spotlight,
 };
